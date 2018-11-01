d670e3757f1fcbeb7301736201dc337951b41aa1
Raul Jimenez <raul.jimenez@mavs.uta.edu>
2018-11-01 13:24:02 -0500

    formatting

--- store.h
+++ store.h
@@ -3,17 +3,19 @@
 #include "product.h"
 #include "vector"
 
-class Store{
-    public:
-        Store(std::string name);
-        std::string name();
-        std::vector<Product*> products();
-        void add_product(Product* product);
-        int number_of_products();
-        std::string product_to_string(int product);
-    private:
-        std::string _name;
-        std::vector<Product*> _products;
+class Store
+{
+  public:
+    Store(std::string name);
+    std::string name();
+    std::vector<Product *> products();
+    void add_product(Product *product);
+    int number_of_products();
+    std::string product_to_string(int product);
+
+  private:
+    std::string _name;
+    std::vector<Product *> _products;
 };
 
 #endif
