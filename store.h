49577304575e0f96ea45d47a13d906acf59f55d9
Raul Jimenez <raul.jimenez@mavs.uta.edu>
2018-11-01 13:22:33 -0500

    syntax bug correction

--- store.h
+++ store.h
@@ -1,18 +1,19 @@
 #ifndef STORE_H
 #define STORE_H
 #include "product.h"
+#include "vector"
 
 class Store{
     public:
         Store(std::string name);
         std::string name();
-        std::vector<Products*> products();
-        void add_product(product* product);
+        std::vector<Product*> products();
+        void add_product(Product* product);
         int number_of_products();
         std::string product_to_string(int product);
     private:
         std::string _name;
-        std::vector<Products*> _products;
+        std::vector<Product> _products;
 };
 
 #endif
