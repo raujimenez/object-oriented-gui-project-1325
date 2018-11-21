2f73bcb80dc745b5298a9bc658876c7576af9bb9
Raul Jimenez <raul.jimenez@mavs.uta.edu>
2018-11-20 19:19:47 -0600

    cut empty space

--- donut.cpp
+++ donut.cpp
@@ -11,41 +11,23 @@ std::string Donut::to_string()
     std::string frosting_string; //get string for frosting
     switch (frosting())
     {
-    case (Frosting::Unfrosted):
-        frosting_string = "Unfrosted";
-        break;
-    case (Frosting::Chocolate_top):
-        frosting_string = "Chocolate top";
-        break;
-    case (Frosting::Vanilla_top):
-        frosting_string = "Vanilla top";
-        break;
-    case (Frosting::Pink_top):
-        frosting_string = "Pink top";
-        break;
+    case (Frosting::Unfrosted): frosting_string = "Unfrosted"; break;
+    case (Frosting::Chocolate_top): frosting_string = "Chocolate top"; break;
+    case (Frosting::Vanilla_top): frosting_string = "Vanilla top"; break;
+    case (Frosting::Pink_top): frosting_string = "Pink top"; break;
     }
 
     std::string sprinkle_string; //get string for sprinkle
-    if (sprinkles())
-        sprinkle_string = "With";
-    else
-        sprinkle_string = "Without";
+    if (sprinkles()) sprinkle_string = "With";
+    else sprinkle_string = "Without";
 
     std::string filling_string; //get string for filling
     switch (filling())
     {
-    case (Filling::Unfilled):
-        filling_string = "Unfilled";
-        break;
-    case (Filling::Creme):
-        filling_string = "Creme";
-        break;
-    case (Filling::Bavarian):
-        filling_string = "Bavarian";
-        break;
-    case (Filling::Strawberry):
-        filling_string = "Strawberry";
-        break;
+    case (Filling::Unfilled): filling_string = "Unfilled"; break;
+    case (Filling::Creme): filling_string = "Creme"; break;
+    case (Filling::Bavarian): filling_string = "Bavarian"; break;
+    case (Filling::Strawberry): filling_string = "Strawberry"; break;
     }
     return name() + " $" + std::to_string(price()) + " $" + std::to_string(cost()) + ", Sprinkles: " + sprinkle_string + ", Filling: " + filling_string + ", Frosting: " + frosting_string;
 }
